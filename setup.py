6b8f52b483b2fb70451d591ea1498179d67f25e8
Aelita-S <1225438527@qq.com>
2022-09-03 01:50:44 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="django-rest-captcha",
+    version="0.3.0",
+    author="evgeny.zuev",
+    author_email="zueves@gmail.com",
+    description="Lightweight version of django-simple-captcha for work with django-rest-framework",
+
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'djangorestframework>=3.5',
+        'django',
+        'pillow>=4.3.0'
+    ]
+)
